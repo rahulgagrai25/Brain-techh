2e27c858d3f698f4142954ed9334dcea04fd11da
rahulgagrai25 <rahulgagraicit369@gmail.com>
2025-11-07 12:36:13 +0530

    updated info

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -367,6 +367,7 @@ function Footer() {
                 <li><a href="/about">About Us</a></li>
                 <li><a href="/services">Our Services</a></li>
                 <li><a href="/distillation">Distillation Process</a></li>
+                <li><a href="/careers">Careers</a></li>
                 <li><a href="/contact">Contact Us</a></li>
               </ul>
             </div>
