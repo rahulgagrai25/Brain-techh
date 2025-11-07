f84c6f054638ff4becde05491a9fb8306b5caf86
rahulgagrai25 <rahulgagraicit369@gmail.com>
2025-11-07 12:40:47 +0530

    Updated layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
+import Hero2 from "./components/Hero2";
+import About from "./components/About";
+import Services from "./components/Services";
+import Choose from "./components/Choose";
+import Client from "./components/Client";
+import Team from "./components/Team";
+import Award from "./components/Award";
 
 export default function Home() {
   return (
     <>
+        <Hero2/>
+        <About/>
+        <Services/>
+        <Choose/>
+        <Award/>
+        <Client/>
     </>
   );
 }
